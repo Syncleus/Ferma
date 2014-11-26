020606567ad5906aaebd29c81b16d6525bcfac76
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2014-11-26 17:46:39 -0500

    Moved most of the content of the AnnotationTypeResolver into the AnnotationTypeFactory class.
    Change-Id: I4086f008d95a736f881c3d66c92b021a327e46f7

--- src/main/java/com/syncleus/ferma/annotations/AnnotationFrameFactory.java
+++ src/main/java/com/syncleus/ferma/annotations/AnnotationFrameFactory.java
@@ -19,29 +19,114 @@
 package com.syncleus.ferma.annotations;
 
 import com.syncleus.ferma.FrameFactory;
+import com.syncleus.ferma.FramedEdge;
+import com.syncleus.ferma.FramedVertex;
 import com.syncleus.ferma.ReflectionCache;
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Element;
+import com.tinkerpop.blueprints.Vertex;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassLoadingStrategy;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.instrumentation.FieldAccessor;
+import net.bytebuddy.modifier.FieldManifestation;
+import net.bytebuddy.modifier.Visibility;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.*;
 
 public class AnnotationFrameFactory implements FrameFactory {
+    private final Map<Class<? extends Annotation>, MethodHandler> methodHandlers = new HashMap<>();
+    private final Map<Class, Class> constructedClassCache = new HashMap<>();
     private final ReflectionCache reflectionCache;
 
     public AnnotationFrameFactory(final ReflectionCache reflectionCache) {
         this.reflectionCache = reflectionCache;
+
+        final PropertyMethodHandler propertyHandler = new PropertyMethodHandler();
+        methodHandlers.put(propertyHandler.getAnnotationType(), propertyHandler);
+
+        final InVertexMethodHandler inVertexHandler = new InVertexMethodHandler();
+        methodHandlers.put(inVertexHandler.getAnnotationType(), inVertexHandler);
+
+        final OutVertexMethodHandler outVertexHandler = new OutVertexMethodHandler();
+        methodHandlers.put(outVertexHandler.getAnnotationType(), outVertexHandler);
+
+        final AdjacencyMethodHandler adjacencyHandler = new AdjacencyMethodHandler();
+        methodHandlers.put(adjacencyHandler.getAnnotationType(), adjacencyHandler);
+
+        final IncidenceMethodHandler incidenceHandler = new IncidenceMethodHandler();
+        methodHandlers.put(incidenceHandler.getAnnotationType(), incidenceHandler);
     }
 
     @Override
-    public <T> T create(final Element e, final Class<T> kind) {
+    public <T> T create(final Element element, final Class<T> kind) {
+
+        Class<? extends T> resolvedKind = kind;
+        if( isAbstract(resolvedKind) )
+            resolvedKind = constructClass(element, kind);
         try {
-            T object = kind.newInstance();
-            if( object instanceof CachesReflection)
-                ((CachesReflection)object).setReflectionCache(this.reflectionCache);
+            T object = resolvedKind.newInstance();
+            if (object instanceof CachesReflection)
+                ((CachesReflection) object).setReflectionCache(this.reflectionCache);
             return object;
         } catch (InstantiationException caught) {
             throw new IllegalArgumentException("kind could not be instantiated", caught);
         } catch (IllegalAccessException caught) {
             throw new IllegalArgumentException("kind could not be instantiated", caught);
         }
+    }
+
+    private static final boolean isAbstract(final Class<?> clazz) {
+        return Modifier.isAbstract(clazz.getModifiers());
+    }
+
+    private static final boolean isAbstract(final Method method) {
+        return Modifier.isAbstract(method.getModifiers());
+    }
+
+    private final <E> Class<? extends E> constructClass(final Element element, final Class<E> clazz) {
+        Class constructedClass = constructedClassCache.get(clazz);
+        if(constructedClass != null )
+            return constructedClass;
+
+        DynamicType.Builder<? extends E> classBuilder;
+        if( clazz.isInterface() ) {
+            if( element instanceof Vertex)
+                classBuilder = (DynamicType.Builder<? extends E>) new ByteBuddy().withImplementing(clazz).subclass(FramedVertex.class);
+            else if( element instanceof Edge)
+                classBuilder = (DynamicType.Builder<? extends E>) new ByteBuddy().withImplementing(clazz).subclass(FramedEdge.class);
+            else
+                throw new IllegalStateException("class is neither an Edge or a vertex!");
+        }
+        else {
+            if( element instanceof Vertex && ! FramedVertex.class.isAssignableFrom(clazz) )
+                throw new IllegalStateException(clazz.getName() + " Class is not a type of FramedVertex");
+            if( element instanceof Edge && ! FramedEdge.class.isAssignableFrom(clazz) )
+                throw new IllegalStateException(clazz.getName() + " Class is not a type of FramedEdge");
+            classBuilder = (DynamicType.Builder<? extends E>) new ByteBuddy().subclass(clazz);
+        }
+
+        classBuilder = classBuilder.defineField("reflectionCache", ReflectionCache.class, Visibility.PRIVATE, FieldManifestation.PLAIN).implement(CachesReflection.class).intercept(FieldAccessor.ofBeanProperty());
+
+        //try and construct any abstract methods that are left
+        for(final Method method : clazz.getMethods() ) {
+            if( isAbstract(method) ) {
+                annotation_loop:
+                for (final Annotation annotation : method.getAnnotations()) {
+                    final MethodHandler handler = methodHandlers.get(annotation.annotationType());
+                    if (handler != null) {
+                        classBuilder = handler.processMethod(classBuilder, method, annotation);
+                        break annotation_loop;
+                    }
+                }
+            }
+        }
+
+        constructedClass = classBuilder.make().load(AnnotationTypeResolver.class.getClassLoader(), ClassLoadingStrategy.Default.WRAPPER).getLoaded();
+        this.constructedClassCache.put(clazz, constructedClass);
+        return constructedClass;
     }
 }
--- src/main/java/com/syncleus/ferma/annotations/AnnotationTypeResolver.java
+++ src/main/java/com/syncleus/ferma/annotations/AnnotationTypeResolver.java
@@ -38,10 +38,7 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 public class AnnotationTypeResolver implements TypeResolver {
-    private final Map<Class<? extends Annotation>, MethodHandler> methodHandlers = new HashMap<>();
-    private final Map<Class, Class> constructedClassCache = new HashMap<>();
     private final ReflectionCache reflectionCache;
-
     /**
      * Creates a new GrailTypeResolver with a typing engine that can recognize the specified types. While these types
      * still need to be included in a separate TypedModule they must be created here as well to ensure proper look-ups
@@ -51,45 +48,20 @@ public class AnnotationTypeResolver implements TypeResolver {
      */
     public AnnotationTypeResolver(final ReflectionCache reflectionCache) {
         this.reflectionCache = reflectionCache;
-
-        final PropertyMethodHandler propertyHandler = new PropertyMethodHandler();
-        methodHandlers.put(propertyHandler.getAnnotationType(), propertyHandler);
-
-        final InVertexMethodHandler inVertexHandler = new InVertexMethodHandler();
-        methodHandlers.put(inVertexHandler.getAnnotationType(), inVertexHandler);
-
-        final OutVertexMethodHandler outVertexHandler = new OutVertexMethodHandler();
-        methodHandlers.put(outVertexHandler.getAnnotationType(), outVertexHandler);
-
-        final AdjacencyMethodHandler adjacencyHandler = new AdjacencyMethodHandler();
-        methodHandlers.put(adjacencyHandler.getAnnotationType(), adjacencyHandler);
-
-        final IncidenceMethodHandler incidenceHandler = new IncidenceMethodHandler();
-        methodHandlers.put(incidenceHandler.getAnnotationType(), incidenceHandler);
     }
 
     @Override
     public <T> Class<T> resolve(final Element element, final Class<T> kind) {
         final String nodeClazz = element.getProperty("implementation_type");
-        if( nodeClazz == null ) {
-            if (!isAbstract(kind))
+        if( nodeClazz == null )
                 return kind;
-            else
-                return (Class<T>) constructClass(element, kind);
-        }
 
         Class<T> nodeKind = (Class<T>) this.reflectionCache.forName(nodeClazz);
 
-        Class<T> resolvedKind;
         if(kind.isAssignableFrom(nodeKind) || kind.equals(FramedVertex.class) || kind.equals(FramedEdge.class) || kind.equals(Object.class))
-            resolvedKind = nodeKind;
+            return nodeKind;
         else
-            resolvedKind = kind;
-
-        if( ! isAbstract(resolvedKind) )
-            return resolvedKind;
-        else
-            return (Class<T>) constructClass(element, resolvedKind);
+            return kind;
     }
 
     @Override
@@ -98,56 +70,5 @@ public class AnnotationTypeResolver implements TypeResolver {
         if (clazz == null) {
             element.setProperty("implementation_type", kind.getName());
         }
-    }
-
-    private static final boolean isAbstract(final Class<?> clazz) {
-        return Modifier.isAbstract(clazz.getModifiers());
-    }
-
-    private static final boolean isAbstract(final Method method) {
-        return Modifier.isAbstract(method.getModifiers());
-    }
-
-    private final <E> Class<? extends E> constructClass(final Element element, final Class<E> clazz) {
-        Class constructedClass = constructedClassCache.get(clazz);
-        if(constructedClass != null )
-            return constructedClass;
-
-        DynamicType.Builder<? extends E> classBuilder;
-        if( clazz.isInterface() ) {
-            if( element instanceof Vertex)
-                classBuilder = (DynamicType.Builder<? extends E>) new ByteBuddy().withImplementing(clazz).subclass(FramedVertex.class);
-            else if( element instanceof Edge)
-                classBuilder = (DynamicType.Builder<? extends E>) new ByteBuddy().withImplementing(clazz).subclass(FramedEdge.class);
-            else
-                throw new IllegalStateException("class is neither an Edge or a vertex!");
-        }
-        else {
-            if( element instanceof Vertex && ! FramedVertex.class.isAssignableFrom(clazz) )
-                throw new IllegalStateException(clazz.getName() + " Class is not a type of FramedVertex");
-            if( element instanceof Edge && ! FramedEdge.class.isAssignableFrom(clazz) )
-                throw new IllegalStateException(clazz.getName() + " Class is not a type of FramedEdge");
-            classBuilder = (DynamicType.Builder<? extends E>) new ByteBuddy().subclass(clazz);
-        }
-
-        classBuilder = classBuilder.defineField("reflectionCache", ReflectionCache.class, Visibility.PRIVATE, FieldManifestation.PLAIN).implement(CachesReflection.class).intercept(FieldAccessor.ofBeanProperty());
-
-        //try and construct any abstract methods that are left
-        for(final Method method : clazz.getMethods() ) {
-            if( isAbstract(method) ) {
-                annotation_loop:
-                for (final Annotation annotation : method.getAnnotations()) {
-                    final MethodHandler handler = methodHandlers.get(annotation.annotationType());
-                    if (handler != null) {
-                        classBuilder = handler.processMethod(classBuilder, method, annotation);
-                        break annotation_loop;
-                    }
-                }
-            }
-        }
-
-        constructedClass = classBuilder.make().load(AnnotationTypeResolver.class.getClassLoader(), ClassLoadingStrategy.Default.WRAPPER).getLoaded();
-        this.constructedClassCache.put(clazz, constructedClass);
-        return constructedClass;
     }
 }

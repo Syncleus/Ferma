822ada0080d038f786c1da1e4d1abdd1d6668681
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2017-09-30 10:57:39 -0400

    chore: updated license headers.
    ISSUES CLOSED: #33

--- src/main/java/com/syncleus/ferma/framefactories/DefaultFrameFactory.java
+++ src/main/java/com/syncleus/ferma/framefactories/DefaultFrameFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2004 - 2016 Syncleus, Inc.
+ * Copyright 2004 - 2017 Syncleus, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
